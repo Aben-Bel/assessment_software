2044d10464516f0bae8422c5bf20cfe7eff68cb5
NyraO <99379453+NyraO@users.noreply.github.com>
2023-06-27 20:38:35 +0300

    Made attributes of the input field dynamic and updated the template

--- assessment_software/src/app/TextField-Component/TextField.component.ts
+++ assessment_software/src/app/TextField-Component/TextField.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector:'app-textfield',
@@ -6,5 +6,21 @@ import { Component } from "@angular/core";
 })
 
 export class TextField{
+    // test:any;
+    @Input() id: string = '';
+    @Input() value: string = '';
+    @Input() variant: 'Standard' | 'Filled' | 'Outlined' = 'Standard';
+    @Input() placeholder: string = '';
+    @Input() autofocus: boolean = false;
+    @Input() classes: string = '';
+    @Input() color: string = 'black';
+    @Input() error: boolean = false;
+    @Input() required: boolean = false;
+    @Input() type: string = 'text';
+    @Output() onChange = new EventEmitter<string>();
     
-}
+    handleChange(event:any) {
+        this.onChange.emit(event.target.value);
+        // this.test=event.target.value;
+        }
+    }
