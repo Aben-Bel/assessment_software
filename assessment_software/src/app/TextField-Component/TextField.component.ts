a1145753161aeef708d6cec42ee760c233551765
NyraO <99379453+NyraO@users.noreply.github.com>
2023-06-28 15:22:20 +0300

    change

--- assessment_software/src/app/TextField-Component/TextField.component.ts
+++ assessment_software/src/app/TextField-Component/TextField.component.ts
@@ -18,10 +18,10 @@ export class TextField{
     @Input() error: boolean = false;
     @Input() required: boolean = false;
     @Input() type: string = 'email';
+    @Input() errorMessage: string = "";
     @Output() onChange = new EventEmitter<string>();
     
     handleChange(event:any) {
         this.onChange.emit(event.target.value);
-        // this.test=event.target.value;
         }
     }
