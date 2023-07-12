1e4129cc0352876c1ea237959749caf1e303c1b3
NyraO <99379453+NyraO@users.noreply.github.com>
2023-07-12 13:42:42 +0300

    implemented maximum file size

--- assessment_software/src/app/file-upload-component/file-upload.component.ts
+++ assessment_software/src/app/file-upload-component/file-upload.component.ts
@@ -6,7 +6,21 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./file-upload.component.css']
 })
 export class FileUploadComponent {
+    listOfFiles:any[]=[];
+    
     @Input() type: string = 'file';
     @Input() accept: string = '';
     @Input() styles: string = '';
+    @Input() fileSizeLimit:any;
+    @Input() error:boolean=false;
+    
+    handleFileSize(event:any){
+      this.listOfFiles=event.target.files;
+      for (let file of this.listOfFiles){
+        if (file.size/1000000>=this.fileSizeLimit)
+        this.error= true;
+        else
+        this.error= false;
+      }
+    }
 }
