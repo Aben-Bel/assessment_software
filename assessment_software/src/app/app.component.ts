b3d59e76742375db94a6845c8b8f384d8eb0eb3a
NyraO <99379453+NyraO@users.noreply.github.com>
2023-07-13 12:54:46 +0300

    addressed reviews

--- assessment_software/src/app/app.component.ts
+++ assessment_software/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assessment_software';
+  fileSelected:any[]=[];
+
+  eventOutput(newFile:any){
+
+    this.fileSelected=newFile;
+    
+  }
 }
