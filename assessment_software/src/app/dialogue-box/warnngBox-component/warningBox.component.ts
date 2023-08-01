1f1c6042c2fe2a75cef5fab9ebcbcabe1aa8f799
NyraO <99379453+NyraO@users.noreply.github.com>
2023-08-01 13:28:07 +0300

    dialogue box event emitter

--- assessment_software/src/app/dialogue-box/warnngBox-component/warningBox.component.ts
+++ assessment_software/src/app/dialogue-box/warnngBox-component/warningBox.component.ts
@@ -7,11 +7,9 @@ styleUrls:['./warningBox.component.css']})
 export class WarningBoxComponent{
     @Input() warning_message:string='Are you sure you want to archive this assignment?';
     @Output() onWarningButtonClick=new EventEmitter;
-    isCancelClicked:boolean=false;
-    handleClick(event: Event) {
-        let htmlElement = <HTMLElement>event.target;
-        if (htmlElement.innerHTML==='Cancel')
-            this.isCancelClicked=true;
+    hideBox:boolean=false;
+    hideDialogue(event: Event) {
+            this.hideBox=true;
     }
     warningButtonClicked(event: Event) {
         this.onWarningButtonClick.emit(event)
