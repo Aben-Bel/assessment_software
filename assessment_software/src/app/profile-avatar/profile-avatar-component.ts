be4594bc5f06f271dca8dbedf46f42797097dbc3
NyraO <99379453+NyraO@users.noreply.github.com>
2023-08-03 18:40:43 +0300

    event emitter

--- assessment_software/src/app/profile-avatar/profile-avatar-component.ts
+++ assessment_software/src/app/profile-avatar/profile-avatar-component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output} from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({selector:'app-profile-avatar',
 templateUrl:'./profile-avatar-component.html',
@@ -6,8 +6,17 @@ styleUrls:['./profile-avatar-component.css']})
 
 export class ProfileAvatarComponent{
     isClicked:boolean=false;
-
-    buttonClicked(){
+    @Input() avatar_name:string='Charlotte';
+    @Output() onPwdClick= new EventEmitter;
+    @Output() onLogoutClick=new EventEmitter;
+    
+    dropClicked(){
         this.isClicked=!(this.isClicked);
+    }
+    handleLogoutClick(event:any){
+        this.onLogoutClick.emit(event);
+    }
+    handleChangePwdClick(event:any){
+        this.onPwdClick.emit(event);
     }
 }
