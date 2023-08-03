d41ada91909547f753f450e19db4dbadeeb9985e
aben-bel <abenezer.sbelay@gmail.com>
2023-08-03 15:04:58 +0300

    implement assigment card

--- assessment_software/src/app/assignment-card/assignment-card.component.ts
+++ assessment_software/src/app/assignment-card/assignment-card.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AssignmentCard } from '../model/assignment-card/assignment-card';
 
 @Component({
   selector: 'app-assignment-card',
   templateUrl: './assignment-card.component.html',
-  styleUrls: ['./assignment-card.component.css']
+  styleUrls: ['./assignment-card.component.css'],
 })
 export class AssignmentCardComponent {
+  @Input() assignment: AssignmentCard = new AssignmentCard(
+    'N/A',
+    'N/A',
+    'N/A',
+    new Date(),
+    'N/A',
+    'N/A',
+    'N/A'
+  );
 
+  @Output() onCardClick = new EventEmitter();
+  @Output() onShareClick = new EventEmitter();
+  @Output() onEditClick = new EventEmitter();
+
+  handleCardClick(event: any) {
+    this.onCardClick.emit(event);
+  }
+
+  handleShareClick(event: any) {
+    event.stopPropagation();
+    this.onShareClick.emit(event);
+  }
+
+  handleEditClick(event: any) {
+    event.stopPropagation();
+    this.onEditClick.emit(event);
+  }
 }
