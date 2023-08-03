6e3b0e2a0858511a3516c2a28f73d59b68729854
aben-bel <abenezer.sbelay@gmail.com>
2023-08-03 13:08:26 +0300

    implement date picker

---
+++ assessment_software/src/app/datepicker/datepicker.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-datepicker',
+  templateUrl: './datepicker.component.html',
+  styleUrls: ['./datepicker.component.css'],
+})
+export class DatepickerComponent {
+  @Input() id: string = '';
+  @Input() variant: string = 'date';
+  @Output() onDateChange = new EventEmitter<string>();
+
+  hangleDateChange(event: any) {
+    this.onDateChange.emit(event.target.value);
+  }
+}
