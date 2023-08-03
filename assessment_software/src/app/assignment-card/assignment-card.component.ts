445065b61837918e54582b8c8518a4560393c6f2
aben-bel <abenezer.sbelay@gmail.com>
2023-08-03 13:36:21 +0300

    create component using angular cli generate command

---
+++ assessment_software/src/app/assignment-card/assignment-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-card',
+  templateUrl: './assignment-card.component.html',
+  styleUrls: ['./assignment-card.component.css']
+})
+export class AssignmentCardComponent {
+
+}
