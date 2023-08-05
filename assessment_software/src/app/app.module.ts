38f41d1913b85f698c98e2fb4a699bacda34223f
NyraO <99379453+NyraO@users.noreply.github.com>
2023-08-05 17:51:16 +0300

    Revert "install Angular npm packages"
    This reverts commit 768d226d28bf2ef7791aac71afab3383233ced37.

--- assessment_software/src/app/app.module.ts
+++ assessment_software/src/app/app.module.ts
@@ -10,16 +10,17 @@ import { FileUploadComponent } from './file-upload-component/file-upload.compone
 import {TextareaComponent} from './textarea-component/textarea.component'
 import { PathDisplayComponent } from './path-display/path-display.component';
 import {TableComponent} from './table-component/table.component';
-import {ProfileAvatarComponent} from './profile-avatar/profile-avatar-component';
 
 import { ConfirmationBoxComponent } from './dialogue-box/confirmationBox-component/confirmationBox.component';
 import { SuccessBoxComponent } from './dialogue-box/successBox-component/successBox.component';
 import {WarningBoxComponent} from './dialogue-box/warnngBox-component/warningBox.component';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { DatepickerComponent } from './datepicker/datepicker.component';
+import {AssignmentCardComponent} from './assignment-card/assignment-card.component';
+
 @NgModule({
   declarations: [AppComponent, TextField, ButtonComponent, FileUploadComponent, TextareaComponent, 
-    PathDisplayComponent, TableComponent, ConfirmationBoxComponent, SuccessBoxComponent, WarningBoxComponent, CourseCardComponent,AssignmentCardComponent, DatepickerComponent,ProfileAvatarComponent],
+    PathDisplayComponent, TableComponent, ConfirmationBoxComponent, SuccessBoxComponent, WarningBoxComponent, CourseCardComponent,AssignmentCardComponent, DatepickerComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
