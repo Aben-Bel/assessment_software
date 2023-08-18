0b7fdd43a0d999bfe062d58a554aac51ddd4d1a8
NyraO <99379453+NyraO@users.noreply.github.com>
2023-08-18 13:51:31 +0300

    pagination style

---
+++ assessment_software/src/app/components/pagination-component/pagination-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination-component',
+  templateUrl: './pagination-component.component.html',
+  styleUrls: ['./pagination-component.component.css']
+})
+export class PaginationComponentComponent {
+
+}
