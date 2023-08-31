acfd8062be05fce1bceec7985a11fd38ddef022f
NyraO <99379453+NyraO@users.noreply.github.com>
2023-08-31 12:41:14 +0300

    pagination changes

--- assessment_software/src/app/components/pagination-component/pagination-component.component.ts
+++ assessment_software/src/app/components/pagination-component/pagination-component.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagination-component.component.css']
 })
 export class PaginationComponentComponent {
-
+/*
+(classes, count(numOfPages), onChange, page(curPage))
+ */
+  classes:string='pagination-container';
 }
